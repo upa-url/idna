a37b4529b9d82c8b073549d68020cf32b3505929
Rimas Misevičius <rmisev3@gmail.com>
2017-06-07 18:18:17 +0300

    Add CodePointRanges class

--- unitool/unitool.cpp
+++ unitool/unitool.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <iostream>
 #include <string>
+#include <vector>
 
 
 static void parse_UnicodeData(const char* file_name, const char* fout_name);
@@ -65,6 +66,77 @@ void OutputFmt::output(const std::string& item) {
 
 OutputFmt::~OutputFmt() {
     m_fout << "]" << std::endl;
+}
+
+// class CodePointRanges
+
+class CodePointRanges {
+public:
+    struct RangeItem {
+        int cp0, cp1;
+        std::string value;
+        RangeItem(int acp0, int acp1, std::string avalue)
+            : cp0(acp0)
+            , cp1(acp1)
+            , value(avalue)
+        {}
+        RangeItem(RangeItem&& src)
+            : cp0(src.cp0)
+            , cp1(src.cp1)
+            , value(std::move(src.value))
+        {}
+    };
+
+    // CodePointRanges();
+    void add(int cp0, int cp1, std::string value);
+    void add(std::string cpstr, std::string value);
+
+protected:
+    std::vector<RangeItem> m_ranges;
+};
+
+void CodePointRanges::add(int cp0, int cp1, std::string value) {
+    if (m_ranges.empty()) {
+        m_ranges.push_back(RangeItem{ cp0, cp1, value });
+    } else {
+        RangeItem& last = m_ranges[m_ranges.size() - 1];
+        if (last.cp1 + 1 == cp0 && last.value == value) {
+            last.cp1 = cp1;
+        } else {
+            m_ranges.push_back(RangeItem{ cp0, cp1, value });
+        }
+    }
+}
+
+inline static int hexstr_to_int(const char* first, const char* last) {
+    int num = 0;
+    for (auto it = first; it != last; it++) {
+        char c = *it;
+        int digit;
+
+        if (c >= '0' && c <= '9')
+            digit = c - '0';
+        else if (c >= 'A' && c <= 'F')
+            digit = c - 'A' + 10;
+        else if (c >= 'a' && c <= 'f')
+            digit = c - 'a' + 10;
+        else
+            throw std::runtime_error("invalid hex number");
+        num = num * 0x10 + digit;
+    }
+    return num;
+}
+
+void CodePointRanges::add(std::string cpstr, std::string value) {
+    size_t pos = cpstr.find("..");
+    if (pos == cpstr.npos) {
+        const int cp0 = hexstr_to_int(cpstr.data(), cpstr.data() + cpstr.length());
+        add(cp0, cp0, value);
+    } else {
+        const int cp0 = hexstr_to_int(cpstr.data(), cpstr.data() + pos);
+        const int cp1 = hexstr_to_int(cpstr.data() + pos + 2, cpstr.data() + cpstr.length());
+        add(cp0, cp1, value);
+    }
 }
 
 // Parse input file
