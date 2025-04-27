85dd918a3c27acf0640a3de479acab4904c53df0
Rimas Misevičius <rmisev3@gmail.com>
2025-04-27 17:26:40 +0300

    Update version to 2.2.0

--- include/upa/idna/idna_version.h
+++ include/upa/idna/idna_version.h
@@ -8,10 +8,10 @@
 // NOLINTBEGIN(*-macro-*)
 
 #define UPA_IDNA_VERSION_MAJOR 2
-#define UPA_IDNA_VERSION_MINOR 1
+#define UPA_IDNA_VERSION_MINOR 2
 #define UPA_IDNA_VERSION_PATCH 0
 
-#define UPA_IDNA_VERSION "2.1.0"
+#define UPA_IDNA_VERSION "2.2.0"
 
 // NOLINTEND(*-macro-*)
 
